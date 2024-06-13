2b0c2c48582d3b81cbcbc84194ecd799d9be3bcd
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-13 13:19:03 +0800

    Create .c

---
+++ .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <signal.h>
+void CDEFGAB(){
+   int C4 = 293.66
+   float D4 = 329.63
+   int E4 = 349.23
+   float F4 = 391.99
+   double G4 = 440.00
+   long double A4 = 493.88
+   long long double B4 = 493.88
+}
