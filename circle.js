196a82f566215288b523be9b6e711a76a45771d4
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-13 12:57:58 +0800

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,10 +1,11 @@
-const Circle1 =
+import gear
+export const Circle1 =
 0
-const Circle2 =
+export const Circle2 =
 0o0
-const CirCle3 =
+export const CirCle3 =
 00o00
-const CirCle4 =
+export const CirCle4 =
 o0o0o 
-const CirCle5 =
+export const CirCle5 =
 o000o
